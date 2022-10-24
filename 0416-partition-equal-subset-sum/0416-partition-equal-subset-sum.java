c322c416b6f4f812033736b6045a0da43afd5c2b
Neel <53505500+Neel630@users.noreply.github.com>
2022-10-24 19:11:17 +0530

    Time: 53 ms (73.59%), Space: 54.4 MB (49.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    Boolean[][] dp;
+    // Boolean[][] dp;
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = 0;
@@ -9,27 +9,42 @@ class Solution {
         
         if((sum&1)!=0) return false;
         
-        dp = new Boolean[n+1][(sum>>1 )+ 1];
+        // return helper(nums, 0, sum>>1);
         
-        return helper(nums, 0, sum>>1);
+        boolean[][] dp = new boolean[n+1][sum/2+1];
         
-//         boolean[][] dp = new boolean[n+1][sum/2+1];
+        for(int i=0;i<n+1;i++){
+            dp[i][0] = true;
+        }
         
-//         for(int i=0;i<n+1;i++){
-//             for(int j=0;j<sum/2+1;j++){
-                                
-//             }
-//         }
+        // for (int j = 1; j < sum+1; j++) {
+        //     dp[0][j] = false;
+        // }
         
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum/2+1;j++){
+                if(nums[i-1] <= j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum>>1];
     }
     
-    public boolean helper(int[] nums, int i, int target){
-        if(target == 0) return true;
+//     public boolean helper(int[] nums, int i, int target){
+//         if(target == 0) return true;
         
-        if(i>= nums.length || target < 0) return false;
+//         if(i>= nums.length || target < 0) return false;
         
-        if(dp[i][target] != null) return dp[i][target];
+//         if(dp[i][target] != null) return dp[i][target];
         
-        return dp[i][target] = helper(nums, i+1, target - nums[i]) || helper(nums, i+1, target);
-    }
+//         if(target >= nums[i]){
+//             return dp[i][target] = helper(nums, i+1, target - nums[i]) || helper(nums, i+1, target);
+//         }
+        
+//         return dp[i][target] = helper(nums, i+1, target);
+//     }
 }
