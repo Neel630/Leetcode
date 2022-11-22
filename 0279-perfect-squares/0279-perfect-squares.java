5f36861aed5c7f64c48c49c952cc19da7b1405a9
Neel <53505500+Neel630@users.noreply.github.com>
2022-11-22 09:34:47 +0530

    Time: 591 ms (9.67%), Space: 44.6 MB (27.60%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,33 @@
+class Solution {
+    Integer[] dp;
+    public int numSquares(int n) {
+        dp = new Integer[n+1];
+        return helper(n);
+    }
+    
+    public int helper(int n){
+        if(n==0) return 0;
+        if(isPerfectSq(n)) return 1;
+        
+        if(n<0) return Integer.MAX_VALUE;
+        
+        if(dp[n] != null) return dp[n];
+        
+        int ans = Integer.MAX_VALUE;
+        for(int i=1;i*i<=n;i++){
+            ans = Math.min(1 + helper(n - (i*i)), ans);
+        }
+        
+        return dp[n] = ans;
+    }
+    
+    public boolean isPerfectSq(int n){
+        if (Math.ceil((double)Math.sqrt(n)) ==
+        Math.floor((double)Math.sqrt(n)))
+        {
+            return true;
+        }
+        return false;
+    
+    }
+}
