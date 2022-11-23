d85f4a09f68c13ac443b49eb1e3ab708a4e97369
Neel <53505500+Neel630@users.noreply.github.com>
2022-11-23 09:02:56 +0530

    Time: 46 ms (70.54%), Space: 43.5 MB (45.47%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,33 +1,55 @@
 class Solution {
-    Integer[] dp;
+    // Integer[] dp;
     public int numSquares(int n) {
-        dp = new Integer[n+1];
-        return helper(n);
-    }
-    
-    public int helper(int n){
-        if(n==0) return 0;
-        if(isPerfectSq(n)) return 1;
+        // dp = new Integer[n+1];
         
-        if(n<0) return Integer.MAX_VALUE;
+        // return helper(n);
         
-        if(dp[n] != null) return dp[n];
+        int[] dp = new int[n+1];
         
-        int ans = Integer.MAX_VALUE;
-        for(int i=1;i*i<=n;i++){
-            ans = Math.min(1 + helper(n - (i*i)), ans);
+        for(int i=1;i<=n;i++){
+            dp[i] = Integer.MAX_VALUE;
         }
         
-        return dp[n] = ans;
-    }
-    
-    public boolean isPerfectSq(int n){
-        if (Math.ceil((double)Math.sqrt(n)) ==
-        Math.floor((double)Math.sqrt(n)))
-        {
-            return true;
+        for(int i=1;i<=n;i++){
+            int sqrt = (int)Math.sqrt(i);
+            
+            if(sqrt*sqrt == i) {
+                dp[i] = 1;
+                continue;
+            }
+            
+            for(int j=1;j<=sqrt;j++){
+                dp[i] = Math.min(dp[i], dp[i-(j*j)] + 1);
+            }
         }
-        return false;
-    
+        
+        return dp[n];
     }
+    
+//     public int helper(int n){
+//         if(n==0) return 0;
+//         if(isPerfectSq(n)) return 1;
+        
+//         if(n<0) return Integer.MAX_VALUE;
+        
+//         if(dp[n] != null) return dp[n];
+        
+//         int ans = Integer.MAX_VALUE;
+//         for(int i=1;i*i<=n;i++){
+//             ans = Math.min(1 + helper(n - (i*i)), ans);
+//         }
+        
+//         return dp[n] = ans;
+//     }
+    
+//     public boolean isPerfectSq(int n){
+//         if (Math.ceil((double)Math.sqrt(n)) ==
+//         Math.floor((double)Math.sqrt(n)))
+//         {
+//             return true;
+//         }
+//         return false;
+    
+//     }
 }
