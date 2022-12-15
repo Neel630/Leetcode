d43bf442b1b0ae8a3a664a1e8479c4aafc5c0288
Neel <53505500+Neel630@users.noreply.github.com>
2022-12-15 10:25:32 +0530

    Time: 21 ms (70.50%), Space: 52.8 MB (7.40%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,24 +1,38 @@
 class Solution {
-    // Integer[][] dp;
+    Integer[][] dp;
     public int longestCommonSubsequence(String text1, String text2) {
-        // dp = new Integer[text1.length()+1][text2.length()+1];
+        dp = new Integer[text1.length()+1][text2.length()+1];
 
-        // return helper(text1, text2, text1.length()-1, text2.length()-1);
+        return helper(text1, text2, text1.length()-1, text2.length()-1);
         
-        int[][] dp = new int[text1.length()+1][text2.length()+1];
+//         int[][] dp = new int[text1.length()+1][text2.length()+1];
         
-        for(int i=1;i<text1.length()+1;i++){
-            for(int j=1;j<text2.length()+1;j++){
-                if(text1.charAt(i-1) == text2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }   
-        }
+//         for(int i=1;i<text1.length()+1;i++){
+//             for(int j=1;j<text2.length()+1;j++){
+//                 if(text1.charAt(i-1) == text2.charAt(j-1)){
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                 }else{
+//                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+//                 }
+//             }   
+//         }
         
-        return dp[text1.length()][text2.length()];
+//         return dp[text1.length()][text2.length()];
     }
+    
+    public int helper(String text1, String text2, int i, int j) {
+    if(i<0 || j<0) return 0;
+    
+    if(dp[i][j]!=null) return dp[i][j];
+        
+    if(text1.charAt(i) == text2.charAt(j)) return 1 + helper(text1, text2, i-1, j-1);
+    
+    return dp[i][j] = Math.max(
+        helper(text1, text2, i-1, j),
+        helper(text1, text2, i, j-1)
+    );
+}
+
     
 //     public int helper(String s1, String s2, int i, int j){
 //         if(i<0 || j<0) return 0;
@@ -30,3 +44,80 @@ class Solution {
 //         return dp[i][j] = Math.max(helper(s1,s2,i-1,j), helper(s1,s2,i,j-1));
 //     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
